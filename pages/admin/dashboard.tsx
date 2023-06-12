b46097c2fd8ec70cac04d3f6da3585bf22bca9d5
unknown <aditalmahdi@gmail.com>
2023-06-12 09:15:35 +0700

    Layouting Dashboard Page (#1)

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { useContext } from 'react'
 import Head from 'next/head'
+import { UserContext } from '@/context/UserContext'
 import AdminLayout from '@/layouts/AdminLayout'
 
 Dashboard.layout = AdminLayout
 
 export default function Dashboard() {
+    const { email, name } = useContext(UserContext)
+
     return (
         <>
         
@@ -12,9 +15,26 @@ export default function Dashboard() {
             <title>Dashboard</title>
         </Head>
 
-        <section id="dashboard-admin-page" className="container my-8 p-3">
+        <section id="dashboard-admin-page" className="container my-4 p-3">
 
-            <h1>Dashboard Page</h1>
+            <h1 className="text-xl font-bold mb-4">Dashboard Page</h1>
+
+            <div className="flex flex-col lg:flex-row md:flex-row justify-between gap-8">
+                <div className="card w-full bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title mb-4">Profile</h2>
+                        <p>{ name }</p>
+                        <p>{ email }</p>
+                    </div>
+                </div>
+                
+                <div className="card lg:w-96 w-full bg-base-100 shadow-xl">
+                    <div className="card-body text-center">
+                        <h2 className="card-title mx-auto">Total Posts</h2>
+                        <p className="my-8 text-5xl">0</p>
+                    </div>
+                </div>
+            </div>
 
         </section>
 
