f13902b94b98cea5c26232d3ade33d407c524a1f
unknown <aditalmahdi@gmail.com>
2023-06-13 11:44:16 +0700

    add features form add item

---
+++ pages/admin/addItem.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Cookies from 'js-cookie'
+import UrlApi from '@/config/urlApi'
+import AdminLayout from '@/layouts/AdminLayout'
+import axios from 'axios'
+
+AddItem.layout = AdminLayout
+
+export default function AddItem() {
+    const userAuth = Cookies.get("token_admin")
+
+    const [name, setName] = useState<string>("")
+    const [price, setPrice] = useState<number | null>(null)
+    const [errors, setErrors] = useState<Record<string, string>>({})
+    const [status, setStatus] = useState<boolean>(false)
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+      
+        setErrors({})
+      
+        await axios
+          .post(`${UrlApi.data_api}items/add`, { name, price }, { headers: { "Authorization": "Bearer " + userAuth } })
+          .then((res) => {
+            setStatus(true)
+            setName("")
+            setPrice(null)
+          })
+          .catch((error) => {
+            setStatus(false)
+            if (error.response && error.response.data && error.response.data.errors) {
+                const apiErrors = error.response.data.errors
+                const fieldErrors: Record<string, string> = {}
+                apiErrors.forEach((apiError: any) => {
+                    fieldErrors[apiError.field] = apiError.message
+                })
+                setErrors(fieldErrors)
+            } else {
+                setErrors({ error: 'An error occurred while adding the item.' })
+            }
+        })
+    }      
+
+    return (
+        <>
+
+            <Head>
+                <title>Add Item</title>
+            </Head>
+
+            <section id="add-item-page-admin" className="container my-4 p-3">
+
+                <h1 className="text-2xl font-bold my-4">Add Item</h1>
+
+                { status && (   
+                    <div className="alert alert-success my-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                        <span>Your purchase has been confirmed!</span>
+                    </div>
+                ) }
+
+                <div className="card bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-control mb-4">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+
+                                <input type="text" placeholder="Type here" className="input input-bordered" name="name" onChange={(e) => setName(e.target.value)} value={name} />
+                                {errors && errors.name && <div className="text-red-500">{errors.name}</div>}
+                            </div>
+
+                            <div className="form-control mb-4">
+                                <label className="label">
+                                    <span className="label-text">Price</span>
+                                </label>
+
+                                <input type="number" placeholder="Type here" className="input input-bordered" name="price" onChange={(e) => setPrice(e.target.value ? Number(e.target.value) : null)} value={price !== null ? price.toString() : ''} />
+                                {errors && errors.price && <div className="text-red-500">{errors.price}</div>}
+                            </div>
+
+                            <div className="flex justify-between">
+                                <Link href="/admin/items" className="btn btn-error">Back</Link>
+                                <button className="btn btn-primary" type="submit">Add</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+
+            </section>
+
+        </>
+    )
+}
--- pages/admin/items.tsx
+++ pages/admin/items.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 import axios from 'axios'
 import AdminLayout from '@/layouts/AdminLayout'
 import formatDistanceToNow from "date-fns/formatDistanceToNow"
@@ -10,8 +11,9 @@ Items.layout = AdminLayout
 
 export default function Items() {
     const [data, setData] = useState<any>("")
-    const [loading, setLoading] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(true)
     const [sc, setSc] = useState("")
+    const [error, setError] = useState<string>("")
 
     const userAuth = Cookies.get("token_admin")
 
@@ -22,22 +24,24 @@ export default function Items() {
             setData(res.data.items)
             setLoading(false)
         }).catch((error) => {
+            setLoading(false)
             alert(error.messsage)
+            setError("An error occurred while fetching the items.")
         })
 
     }, [])
 
-    let searchItems = () => {
-        let searchParam = ["name"]
-
+    const searchItems = () => {
+        const searchParam = ["name", "code"]
+      
         return data?.filter((dt: any) => {
-            return searchParam.some((newData) => {
-                return (
-                    dt[newData].toString().toLowerCase().indexOf(sc.toLowerCase()) > -1
-                )
-            })
+          return searchParam.some((newData) => {
+            return (
+              dt[newData]?.toString().toLowerCase().indexOf(sc.toLowerCase()) > -1
+            )
+          })
         })
-    }
+    }      
 
     return (
         <>
@@ -49,42 +53,46 @@ export default function Items() {
         <section id="items-admin-page" className="container my-4 p-3">
             
             <div className="mb-4 flex justify-between">
-                <button className="btn btn-primary">Add</button>
+                <Link href="/admin/addItem" className="btn btn-primary">Add</Link>
 
-                <input type="text" placeholder="Search Items ..." className="input input-bordered lg:w-96 md:w-96" name="search-items" />
+                <input type="text" placeholder="Search Items ..." className="input input-bordered lg:w-96 md:w-96" name="search-items" onChange={(e) => setSc(e.target.value)} value={sc} />
             </div>
 
             <h1 className="my-4 text-center font-bold text-2xl">Items</h1>
 
-            <div className="overflow-x-auto my-8">
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th></th> 
-                        <th>Code</th> 
-                        <th>Name</th> 
-                        <th>Price</th> 
-                        <th>Updated At</th>
-                        <th>Action</th>
-                    </tr>
-                    </thead> 
-                    <tbody>
-                        { data && searchItems().map((item: any, index: number) => (
-                            <tr key={index}>
-                                <th>{ index + 1 }</th> 
-                                <td>{ item.code }</td> 
-                                <td>{ item.name }</td> 
-                                <td>{ item.price }</td> 
-                                <td>{ formatDistanceToNow(new Date(item.updated_at), { addSuffix: true }) }</td>
-                                <td className="flex gap-4">
-                                    <span className="badge badge-success">Edit</span>
-                                    <span className="badge badge-error cursor-pointer">Delete</span>
-                                </td>
-                            </tr>
-                        )) }
-                    </tbody>
-                </table>
-            </div>
+            { loading && <p className="text-current font-light">Loading...</p> }
+            { error && <p className="text-red-500">{error}</p> }
+            { !loading && !error && (
+                <div className="overflow-x-auto my-8">
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th></th> 
+                            <th>Code</th> 
+                            <th>Name</th> 
+                            <th>Price</th> 
+                            <th>Updated At</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead> 
+                        <tbody>
+                            { data && searchItems().map((item: any, index: number) => (
+                                <tr key={index}>
+                                    <th>{ index + 1 }</th> 
+                                    <td>{ item.code }</td> 
+                                    <td>{ item.name }</td> 
+                                    <td>{ item.price }</td> 
+                                    <td>{ formatDistanceToNow(new Date(item.updated_at), { addSuffix: true }) }</td>
+                                    <td className="flex gap-4">
+                                        <span className="badge badge-success">Edit</span>
+                                        <span className="badge badge-error cursor-pointer">Delete</span>
+                                    </td>
+                                </tr>
+                            )) }
+                        </tbody>
+                    </table>
+                </div>
+            ) }
 
         </section>
 
